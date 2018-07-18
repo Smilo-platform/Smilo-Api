cd7634556c003b338b068c590cc94a7d408be78b
Unknown <mail@mathyn.com>
2018-07-18 13:45:29 +0200

    Transactions can now be send to the blockchain

--- src/main/java/io/smilo/api/rest/service/TransactionServiceImpl.java
+++ src/main/java/io/smilo/api/rest/service/TransactionServiceImpl.java
@@ -2,6 +2,7 @@ package io.smilo.api.rest.service;
 
 import io.smilo.api.block.data.transaction.Transaction;
 import io.smilo.api.peer.PeerClient;
+import io.smilo.api.peer.PeerSender;
 import io.smilo.api.peer.payloadhandler.PayloadType;
 import io.smilo.api.pendingpool.PendingBlockDataPool;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +11,14 @@ import org.springframework.stereotype.Service;
 @Service("transactionService")
 public class TransactionServiceImpl implements TransactionService {
     @Autowired
-    PeerClient peerClient;
+    PeerSender peerSender;
 
     @Autowired
     PendingBlockDataPool dataPool;
 
     @Override
     public Transaction putTransaction(Transaction transaction) {
-        // Do we use this method to broadcast to peers?
-        // More importantly: how do we format the transaction?
-        dataPool.addBlockData(transaction);
+        peerSender.broadcastContent(transaction);
 
         return transaction;
     }
