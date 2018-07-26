0d8967b475c60dfb3625eb4ebf3d831c3fa3ff5d
Unknown <mail@mathyn.com>
2018-07-26 16:52:51 +0200

    Continued working on tests for the TxController

--- src/test/java/io/smilo/api/rest/TxTests.java
+++ src/test/java/io/smilo/api/rest/TxTests.java
@@ -1,5 +1,30 @@
 package io.smilo.api.rest;
 
-public class TxTests {
+import io.smilo.api.AbstractWebSpringTest;
+import org.apache.commons.lang3.NotImplementedException;
+import org.junit.Test;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+public class TxTests extends AbstractWebSpringTest {
+    @Test
+    public void shouldReturn404WhenRequestingUnknownTransaction() throws Exception {
+        this.webClient.perform(MockMvcRequestBuilders.get("/tx/unknown_tx_hash"))
+                .andExpect(MockMvcResultMatchers.status().is(404));
+    }
+
+    @Test
+    public void shouldReturn200WhenRequestingKnownTransaction() throws Exception {
+        throw new NotImplementedException("Not implemented");
+    }
+
+    @Test
+    public void shouldReturnCorrectTransaction() throws Exception {
+        throw new NotImplementedException("Not implemented");
+    }
+
+    @Test
+    public void shouldReturnCorrectListOfTransactions() throws Exception {
+        throw new NotImplementedException("Not implemented");
+    }
 }
