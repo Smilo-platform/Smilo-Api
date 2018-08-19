6682872fb8b9244b19a3888b1dd16c9fbd312417
Thomas Modeneis <thomas.modeneis@gmail.com>
2018-08-19 23:15:45 +0200

    refactor using smilo-commons

--- src/test/java/io/smilo/api/TestUtility.java
+++ src/test/java/io/smilo/api/TestUtility.java
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package io.smilo.api;
@@ -22,21 +21,32 @@ import io.smilo.api.peer.MockPeer;
 import io.smilo.api.peer.PeerBuilder;
 import io.smilo.commons.block.Block;
 import io.smilo.commons.block.BlockParser;
+import io.smilo.commons.block.BlockStore;
 import io.smilo.commons.block.data.transaction.Transaction;
 import io.smilo.commons.block.data.transaction.TransactionOutput;
 import io.smilo.commons.block.data.transaction.TransactionParser;
-import io.smilo.commons.peer.IPeer;
+import io.smilo.commons.block.genesis.GenesisLoader;
+import io.smilo.commons.peer.PeerClient;
+import io.smilo.commons.peer.PeerStore;
+import io.smilo.commons.peer.network.NetworkConsensus;
+import io.smilo.commons.peer.network.link.NetworkLinker;
 import io.smilo.commons.peer.payloadhandler.BlockHandler;
-import org.mockito.Mock;
+import io.smilo.commons.peer.sport.NetworkState;
+import io.smilo.commons.peer.sport.SportState;
+import io.smilo.commons.pendingpool.PendingBlockDataPool;
+import io.smilo.commons.db.Store;
+import io.smilo.commons.ledger.AddressManager;
+import io.smilo.commons.ledger.LedgerManager;
+import io.smilo.commons.ledger.LedgerStore;
+import io.smilo.commons.ledger.PrivateKeyGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.File;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Component
 public class TestUtility {
@@ -46,6 +56,45 @@ public class TestUtility {
 
     @Value("${DB_FOLDER:database}")
     private String databaseDirName;
+
+    @Autowired
+    private AddressManager addressManager;
+
+    @Autowired
+    private LedgerStore ledgerStore;
+
+    @Autowired
+    private LedgerManager ledgerManager;
+
+    @Autowired
+    private GenesisLoader genesis;
+
+    @Autowired
+    private SportState sportState;
+
+    @Autowired
+    private PendingBlockDataPool pendingBlockDataPool;
+
+    @Autowired
+    private BlockStore blockStore;
+
+    @Autowired
+    private PeerStore peerStore;
+
+    @Autowired
+    private PeerClient peerClient;
+
+    @Autowired
+    private Store store;
+
+    @Autowired
+    private NetworkState networkState;
+
+    @Autowired
+    private NetworkLinker networkLinker;
+
+    @Autowired
+    private NetworkConsensus networkConsensus;
 
     @Autowired
     private BlockHandler blockHandler;
@@ -59,13 +108,44 @@ public class TestUtility {
     @Autowired
     private BlockParser blockParser;
 
+    private String address;
+    private String privateKey;
+
     /**
      * Initialize the environment. Some classes need to be reset to their original state after every test.
      */
     public void initialize() {
-        // TODO: incomplete; block store needs to be regenerated as well
+
         cleanUp();
         createFolders();
+
+        ReflectionTestUtils.setField(addressManager, "privateKeyGenerator", new  PrivateKeyGenerator() {
+            @Override
+            public String getPrivateKey() {
+                return "supersecretkey";
+            }
+        });
+
+        ReflectionTestUtils.setField(blockStore, "chains", new ArrayList<>());
+
+
+        // Retrieve the initially generated private keys
+        List<AbstractMap.SimpleEntry<String,String>> privateKeys = (List<AbstractMap.SimpleEntry<String,String>>) ReflectionTestUtils.getField(addressManager, "privateKeys");
+        // Save the initial address and privateKey to be reused for all tests
+        if (address == null) {
+            address = privateKeys.get(0).getKey();
+            privateKey = privateKeys.get(0).getValue();
+        }
+
+        // Save the generated address and key in the addressManager without generating a new one using the merkle tree
+        // This is a huge performance boost for the unit test suite
+        privateKeys.clear();
+        privateKeys.add(new AbstractMap.SimpleEntry<>(address, privateKey));
+        privateKeys.add(new AbstractMap.SimpleEntry<>("S1RQ3ZVRQ2K42FTXDONQVFVX73Q37JHIDCSFAR","BOdbzfoE9Za9a4cGQTpExBYw7mQNFo2B"));
+
+        genesis.loadGenesis();
+
+        sportState.initialize(addressManager.getDefaultAddress());
     }
 
     /**
@@ -74,6 +154,19 @@ public class TestUtility {
     public void cleanUp() {
         deleteDir(new File(walletFileName));
         deleteDir(new File(databaseDirName));
+        pendingBlockDataPool.getPendingBlockData().clear();
+        ReflectionTestUtils.setField(blockStore, "chains", new ArrayList<>());
+        peerStore.clear();
+        sportState.reset();
+        ledgerManager.getPendingTransactions().clear();
+        ledgerStore.clearAccounts();
+        ReflectionTestUtils.setField(peerClient, "pendingPeers", new HashSet<>());
+        store.clear("block");
+        store.initializeCollection("block");
+        store.initializeCollection("account");
+        ReflectionTestUtils.setField(networkState, "networks", new HashSet<>());
+        ReflectionTestUtils.setField(networkLinker, "networks", new ArrayList<>());
+        ReflectionTestUtils.setField(networkConsensus, "approvedNodes", new HashMap<>());
     }
 
     private void createFolders() {
